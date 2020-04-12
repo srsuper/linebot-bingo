47c87a762e08ee0c8fe78fc951bafeb95b9fa40f
saharei <saharai.co@gmail.com>
2020-04-13 06:39:05 +0900

    追加：DB接続、テーブル名定義

--- index.php
+++ index.php
@@ -2,6 +2,9 @@
 
 // Composerでインストールしたライブラリを一括読み込み
 require_once __DIR__ . '/vendor/autoload.php';
+// テーブル名を定義
+define('TABLE_NAME_SHEETS', 'sheets');
+
 
 // アクセストークンを使いCurlHTTPClientをインスタンス化
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient(getenv('CHANNEL_ACCESS_TOKEN'));
@@ -176,5 +179,35 @@ function replyCarouselTemplate($bot, $replyToken, $alternativeText, $columnArray
     error_log('Failed!'. $response->getHTTPStatus . ' ' . $response->getRawBody());
   }
 }
+
+// データベースへの接続を管理するクラス
+class dbConnection {
+  // インスタンス
+  protected static $db;
+  // コンストラクタ
+  private function __construct() {
+
+    try {
+      // 環境変数からデータベースへの接続情報を取得し
+      $url = parse_url(getenv('DATABASE_URL'));
+      // データソース
+      $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
+      // 接続を確立
+      self::$db = new PDO($dsn, $url['user'], $url['pass']);
+      // エラー時例外を投げるように設定
+      self::$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    }
+    catch (PDOException $e) {
+      error_log('Connection Error: ' . $e->getMessage());
+    }
+  }
   
+  // シングルトン。存在しない場合のみインスタンス化
+  public static function getConnection() {
+    if (!self::$db) {
+      new dbConnection();
+    }
+    return self::$db;
+  }
+}
 ?>
